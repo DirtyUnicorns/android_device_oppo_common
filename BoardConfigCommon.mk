66a6193a47f92f8ff745e18e1f8fefab36554bce
Alex Cruz <mazdarider23@gmail.com>
2016-08-14 22:28:11 -0400

    Clean repo up of things we don't need/use

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,11 +16,6 @@
 
 BOARD_VENDOR := oppo
 
-# CM Hardware
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
-
 # QCOM Power
 TARGET_POWERHAL_VARIANT := qcom
 
